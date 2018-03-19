d3adf26d5f5c2bf1f95d7e928b0133d6c2a2e887
RobinChen <RobinChenjp@outlook.com>
2018-03-19 15:52:05 +0800

    complete basic functions

--- game_main.py
+++ game_main.py
@@ -1,6 +1,9 @@
 import pygame
 import game_sprite
 
+ID_SHOW_ENEMY_SPRITE = pygame.USEREVENT
+ID_SHOW_BULLET_SPRITE = pygame.USEREVENT + 1
+
 
 class PlaneWar(object):
 
@@ -10,17 +13,24 @@ class PlaneWar(object):
         self.clock = pygame.time.Clock()
         self.background_sprite = game_sprite.BackgroundSprite("./images/background.png")
         self.background_sprite_alt = game_sprite.BackgroundSprite("./images/background.png", is_alt=True)
-        self.enemy_sprite = game_sprite.EnemySprite("./images/enemy1.png")
         self.plane_sprite = game_sprite.PlaneSprite("./images/me1.png")
-        self.group = pygame.sprite.Group()
-        self.group.add(self.background_sprite, self.background_sprite_alt, self.plane_sprite, self.enemy_sprite)
+        self.bg_group = pygame.sprite.Group()
+        self.player_group = pygame.sprite.Group()
+        self.enemy_group = pygame.sprite.Group()
+        self.bg_group.add(self.background_sprite, self.background_sprite_alt)
+        self.player_group.add(self.plane_sprite)
+        pygame.time.set_timer(ID_SHOW_ENEMY_SPRITE, 1000)
+        pygame.time.set_timer(ID_SHOW_BULLET_SPRITE, 200)
 
     def start_game(self):
         print("开始游戏")
-        while True:
+        while self.plane_sprite.is_survive:
             self.handle_key_event()
+            self.check_collide()
             self.update_screen()
             self.clock.tick(60)
+        print("游戏结束")
+        PlaneWar.end_game()
 
     @staticmethod
     def end_game():
@@ -31,13 +41,30 @@ class PlaneWar(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 PlaneWar.end_game()
-            else:
-                print("无效按键，KEY_TYPE：" + str(event.type))
+            elif event.type == ID_SHOW_ENEMY_SPRITE:
+                enemy_sprite = game_sprite.EnemySprite("./images/enemy1.png")
+                self.enemy_group.add(enemy_sprite)
+            elif event.type == ID_SHOW_BULLET_SPRITE:
+                bullet_sprite = game_sprite.BulletSprite("./images/bullet1.png", self.plane_sprite)
+                self.player_group.add(bullet_sprite)
+
+        pressed_key = pygame.key.get_pressed()
+        self.plane_sprite.control_direction(pressed_key)
 
     def update_screen(self):
-        self.group.update()
-        self.group.draw(self.screen)
+        self.bg_group.update()
+        self.bg_group.draw(self.screen)
+
+        self.player_group.update()
+        self.player_group.draw(self.screen)
+
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+
         pygame.display.update()
+
+    def check_collide(self):
+        pygame.sprite.groupcollide(self.enemy_group, self.player_group, True, True)
 
 
 if __name__ == "__main__":
--- game_sprite.py
+++ game_sprite.py
@@ -14,41 +14,60 @@ class GameSprite(pygame.sprite.Sprite):
 
 
 class PlaneSprite(GameSprite):
-    def __init__(self, img_path, speed=1):
-        super().__init__(img_path, speed)
-        self.rect.x = (SCREEN_RECT.width - self.rect.width) / 2
-        self.rect.y = SCREEN_RECT.height - self.rect.height
+
+    def __init__(self, img_path):
+        super().__init__(img_path, 0)
+        self.is_survive = True
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 60
 
     def update(self, *args):
-        self.rect.y -= self.speed
-        if self.rect.y < -self.rect.height:
-            self.rect.y = SCREEN_RECT.height
+        pass
+
+    def control_direction(self, pressed_key):
+        if pressed_key[pygame.K_LEFT] and self.rect.x > -20:
+            self.rect.x -= 2
+        elif pressed_key[pygame.K_RIGHT] and self.rect.right < SCREEN_RECT.width + 20:
+            self.rect.x += 2
+        if pressed_key[pygame.K_UP] and self.rect.y > 0:
+            self.rect.y -= 2
+        elif pressed_key[pygame.K_DOWN] and self.rect.bottom < SCREEN_RECT.height:
+            self.rect.y += 2
+
+    def kill(self):
+        super().kill()
+        self.is_survive = False
+
 
 
 class EnemySprite(GameSprite):
-    def __init__(self, img_path, speed=1):
-        super().__init__(img_path, speed)
-        self.max_x = SCREEN_RECT.width - self.rect.width
-        self.random_show()
+    def __init__(self, img_path):
+        super().__init__(img_path, 1)
+        max_x = SCREEN_RECT.width - self.rect.width
+        self.random_show(max_x)
 
     def update(self, *args):
         self.rect.y += self.speed
         if self.rect.y > SCREEN_RECT.height:
-            self.rect.y = -self.rect.height
-            self.random_show()
+            self.kill()
 
-    def random_show(self):
-        self.rect.x = random.randint(0, self.max_x)
+    def random_show(self, max_x):
+        self.rect.x = random.randint(0, max_x)
+        self.rect.bottom = 0
         self.speed = random.randint(1, 3)
 
 
 class BulletSprite(GameSprite):
-    def __init__(self, img_path, speed=1):
-        super().__init__(img_path, speed)
+    def __init__(self, img_path, plane_sprite, ):
+        super().__init__(img_path, 2)
+        self.rect.centerx = plane_sprite.rect.centerx
+        self.rect.bottom = plane_sprite.rect.y
 
     def update(self, *args):
         super().update()
         self.rect.y -= self.speed
+        if self.rect.bottom <= 0:
+            self.kill()
 
 
 class BackgroundSprite(GameSprite):
